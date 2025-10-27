f2ddeb956b4a032a812e5709e397f3e10081c03b
s1140341-boop <s1140341@dmhs.kh.edu.tw>
2025-10-27 16:23:56 +0800

    name

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
+    
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    int age = 16;
+    double height = 172;
+    std::string name = "Lawrence";
+    bool is_student = true;
+    std::cout << "Lawrence " << name << std::endl;
+    std::cout << "172 " << height << std::endl;
+    std::cout << "16 " << age << std::endl;
+    std::cout << "true " << is_student << std::endl;
+    
+    
+    
     return 0;
 }
