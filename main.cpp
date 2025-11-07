2b633b7fdda49a05cee64125e23eba45acd94ecc
s1140341-boop <s1140341@dmhs.kh.edu.tw>
2025-11-07 11:41:08 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
+
+int main()
+{
+    std::cout<<"HappyPlanet" << std::endl;
+    std::cout<<"HappyPlanet" << std::endl;
+    std::cout<<"Happy\tPlanet" << std::endl;
+    std::cout<<"Happy\nPlanet" << std::endl;
+    std::cout<<"Happy\"Planet\"" << std::endl;
+    std::cout<<"Happy\Planet" << std::endl;
     
-int main() {
-    int age = 16;
-    double height = 172;
-    std::string name = "Lawrence";
-    bool is_student = true;
-    std::cout << "Lawrence " << name << std::endl;
-    std::cout << "172 " << height << std::endl;
-    std::cout << "16 " << age << std::endl;
-    std::cout << "true " << is_student << std::endl;
-    
-    
-    
+
     return 0;
 }
